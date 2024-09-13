8c377f886104afe68934de6c8f8b134fe61248e8
tania-n <137098@epsstudents.org>
2024-09-13 14:12:20 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
 public class Main{ 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Enter a string: "); 
+        String string1 = input.nextLine(); 
+        
+        System.out.println("Enter a string: "); 
+        String string2 = input.nextLine(); 
+        
+        System.out.println("Enter a string: "); 
+        String string3 = input.nextLine(); 
+        
+        System.out.println("Enter a string: "); 
+        String string4 = input.nextLine(); 
+        
+        System.out.println("Enter a string: "); 
+        String string5 = input.nextLine(); 
+        
+        System.out.println("Enter a string: "); 
+        String string6 = input.nextLine(); 
+        
+        System.out.println("Enter a string: "); 
+        String string7 = input.nextLine(); 
+        
+        System.out.println("Enter a double: "); 
+        double double1 = input.nextDouble(); 
+        
+        System.out.println("Enter an integer: "); 
+        int integer1 = input.nextInt(); 
+        
+        System.out.println("Enter a boolean: "); 
+        boolean boolean1 = input.nextBoolean(); 
+        
+    }
     
 }
