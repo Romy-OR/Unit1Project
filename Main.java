f876d84668a440652380e6587ae7b6b23faeacdf
tania-n <137098@epsstudents.org>
2024-09-15 22:42:52 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -23,6 +23,15 @@ public class Main
         
       System.out.println("Enter a string: "); 
       String string7 = input.nextLine(); 
+      
+      System.out.println("Enter a string: "); 
+      String string8 = input.nextLine(); 
+      
+      System.out.println("Enter a string: "); 
+      String string9 = input.nextLine(); 
+      
+      System.out.println("Enter a string: "); 
+      String string10 = input.nextLine();
         
       System.out.println("Enter a double: "); 
       double double1 = input.nextDouble(); 
@@ -33,11 +42,11 @@ public class Main
       System.out.println("Enter a boolean: "); 
       boolean boolean1 = input.nextBoolean(); 
         
-      System.out.println("Thank you, ladies and " + string1 + ". My heart is beating at " + int1 + " beats a minute. I didn’t prepare a" + string2 + ".");
-      System.out.println("Even though I have received a lot of praise, and my newest movie " + string 3 + "had a rating of " + double1 + " stars");
-      System.out.println(",I never expected to win this " + string4 + " Oscar. I have so many people to " + string5 + ". First and foremost, my");
-      System.out.println(string6 + " costar——who was always in my dressing " + string7 ", held my " + string8 + " when I was in trouble, and never");
-      System.out.println("failed to compliment me or give me a pat on my " + string9 + " when I did well.  Most of all, I want to thank you, my");
-      System.out.println(string10 + " fans. It is without a doubt" + boolean1 + " that this was not possible without you. Thank you.");  
+      System.out.println("Thank you, ladies and " + string1 + ". My heart is beating at " + integer1 + " beats a minute. I didn’t prepare a " + string2 + ".");
+      System.out.println("Even though I have received a lot of praise, and my newest movie " + string3 + " had a rating of " + double1 + " stars,");
+      System.out.println("I never expected to win this " + string4 + " Oscar. I have so many people to " + string5 + ". First and foremost, my ");
+      System.out.println(string6 + " costar——who was always in my dressing " + string7 + ", held my " + string8 + " when I was in trouble, and never");
+      System.out.println("failed to compliment me or give me a pat on my " + string9 + " when I did well. Most of all, I want to thank you, my");
+      System.out.println(string10 + " fans. It is without a doubt " + boolean1 + " that this was not possible without you. Thank you.");  
   }
 }
